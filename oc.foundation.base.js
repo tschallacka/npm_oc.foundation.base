35652aa36c8e537e409972ca28c33dab95c31694
Michael Dibbets <michael@exit-reizen.nl>
2020-04-30 14:10:16 +0200

    Updated version
    updated version for npm pubishing.
    beautified the code
    added more lets instead of vars

--- oc.foundation.base.js
+++ oc.foundation.base.js
@@ -12,14 +12,15 @@ var Base = function(appName, element, options)
     throw new Error("No appName defined for application");
   }
   this.appID = this.appName.replace(/[^a-z]+/gi, '').replace(/(.)([A-Z])/g, "$1-$2").toLowerCase();
-  this.appDataHandler = '[data-'+this.appID+']';	
+  this.appDataHandler = '[data-'+this.appID+']';  
   this.oc = 'oc.'+this.appName;
   this.$el = $(element); 
-	this.options = options || {};
-	if(element) {
+  this.options = options || {};
+  
+  if(element) {
     controlUtils.markDisposable(element);
     this.sysInit();
-	}
+  }
 };
 
 /**
@@ -27,10 +28,10 @@ var Base = function(appName, element, options)
  */
 Base.prototype.dispose = function() 
 {
-    for (var key in this.proxiedMethods) {
-    	if(this.proxiedMethods.hasOwnProperty(key)) {
-    		this.proxiedMethods[key] = null
-    	}
+    for (let key in this.proxiedMethods) {
+      if(this.proxiedMethods.hasOwnProperty(key)) {
+        this.proxiedMethods[key] = null
+      }
     }
     this.proxiedMethods = null
 };
@@ -40,8 +41,8 @@ Base.prototype.dispose = function()
  * @param method function
  */
 Base.prototype.proxy = function(method) 
-{	
-  if (method.ocProxyId === undefined) {    	
+{  
+  if (method.ocProxyId === undefined) {      
     $.oc.foundation._proxyCounter++
     method.ocProxyId = $.oc.foundation._proxyCounter
   }
@@ -61,8 +62,8 @@ Base.prototype.proxy = function(method)
  */
 Base.prototype.alloc = function(name, value) 
 {
-	this.alloclist.push(name);
-	this[name] = value;
+  this.alloclist.push(name);
+  this[name] = value;
 };
 
 /**
@@ -71,12 +72,12 @@ Base.prototype.alloc = function(name, value)
  */
 Base.prototype.free = function() 
 {
-	for(var c = 0; c < this.alloclist.length; c++) 
-	{
-		var name = this.alloclist[c];
-		this[name] = null;
-	}
-	this.alloclist = null;
+  for(let i = 0; i < this.alloclist.length; i++) 
+  {
+    var name = this.alloclist[i];
+    this[name] = null;
+  }
+  this.alloclist = null;
 };
 
 /**
@@ -90,12 +91,12 @@ Base.prototype.free = function()
  */
 Base.prototype.bind = function(event, $el, callback_or_subselector, callback) 
 {
-	if(typeof callback_or_subselector === 'string') {
-		$el[this.event_binding_type](event, callback_or_subselector, this.proxy(callback));
-	}
-	else {
-		$el[this.event_binding_type](event, this.proxy(callback_or_subselector));
-	}
+  if(typeof callback_or_subselector === 'string') {
+    $el[this.event_binding_type](event, callback_or_subselector, this.proxy(callback));
+  }
+  else {
+    $el[this.event_binding_type](event, this.proxy(callback_or_subselector));
+  }
 };
 
 /**
@@ -104,19 +105,19 @@ Base.prototype.bind = function(event, $el, callback_or_subselector, callback)
  */
 Base.prototype.sysDestroy = function() 
 {
-	this.event_binding_type='off';
-	this.handlers(this.event_binding_type);
-	this.destroy();
-	this.free();
-	this.$el.off('dispose-control', this.proxy(this.dispose));
-    this.$el.removeData(this.oc);
-	this.requesthandle = null;
-    this.$el = null
+  this.event_binding_type='off';
+  this.handlers(this.event_binding_type);
+  this.destroy();
+  this.free();
+  this.$el.off('dispose-control', this.proxy(this.dispose));
+  this.$el.removeData(this.oc);
+  this.requesthandle = null;
+  this.$el = null
 
-    // In some cases options could contain callbacks, 
-    // so it's better to clean them up too.
-    this.options = null
-    Base.prototype.dispose.call(this);
+  // In some cases options could contain callbacks, 
+  // so it's better to clean them up too.
+  this.options = null
+  Base.prototype.dispose.call(this);
 };
 
 /**
@@ -126,14 +127,17 @@ Base.prototype.sysDestroy = function()
  */
 Base.prototype.getHandle = function(name) 
 {
-	if(this.handle) {
-		return this.handle + name;
-	}
-	this.handle = this.$el.data('apphandler');
-	if(this.handle != null && this.handle.match(/::$/) != null) {
-		this.handle += '::';
-	}
-	return this.getHandle(name);
+  if(this.handle) {
+    return this.handle + name;
+  }
+  
+  this.handle = this.$el.data('apphandler');
+  
+  if(this.handle != null && this.handle.match(/::$/) != null) {
+    this.handle += '::';
+  }
+  
+  return this.getHandle(name);
 };
 
 /**
@@ -144,7 +148,7 @@ Base.prototype.getHandle = function(name)
  */
 Base.prototype.request = function(requestname, data) 
 {
-	return this.$el.request(this.getHandle(requestname), data);
+  return this.$el.request(this.getHandle(requestname), data);
 };
 
 /**
@@ -152,11 +156,12 @@ Base.prototype.request = function(requestname, data)
  */
 Base.prototype.sysInit = function() 
 {
-	this.$el.one('dispose-control', this.proxy(this.sysDestroy));
-	this.event_binding_type='on';
-	this.alloclist = [];
-	this.init();
-	this.handlers(this.event_binding_type);
+  this.$el.one('dispose-control', this.proxy(this.sysDestroy));
+  this.event_binding_type='on';
+  this.alloclist = [];
+  
+  this.init();
+  this.handlers(this.event_binding_type);
 };
 
 /**
@@ -172,47 +177,47 @@ Base.prototype.sysInit = function()
  * var Base = require('@tschallacka/oc.foundation.base');
  * Base.bindToRender(require('js/myApplication.js'));
  */
-Base.bindToRender = function(Application) {
-	var app = new Application();
-	var oc = app.oc;
-	var appName = app.appName;
-	var old = $.fn[appName];
-	var appDataHandler = app.appDataHandler;
-	var appVersion = app.appVersion;
-	
-    $.fn[appName] = function (option) {
-        var args = Array.prototype.slice.call(arguments, 1), items, result;
+Base.bindToRender = function(Application) 
+{
+  let app = new Application();
+  let oc = app.oc;
+  let appName = app.appName;
+  let old = $.fn[appName];
+  let appDataHandler = app.appDataHandler;
+  let appVersion = app.appVersion;
+  
+  $.fn[appName] = function (option) {
+    let args = Array.prototype.slice.call(arguments, 1), items, result;
         
-        items = this.each(function (index, elem) {
-            var $this   = $(elem);
-            var data    = $this.data(oc);
-            var options = $.extend({}, Application.DEFAULTS, $this.data(), typeof option == 'object' && option);
-            
-            if (!data) {
-            	$this.data(oc, (data = new Application(this, options)));
-            }
-            if (typeof option == 'string') {
-            	result = data[option].apply(data, args);
-            }            
-            if (typeof result != 'undefined') {
-            	return false;
-            }
-        });
+    items = this.each(function (index, elem) {
+      let $this   = $(elem);
+      let data    = $this.data(oc);
+      let options = $.extend({}, Application.DEFAULTS, $this.data(), typeof option == 'object' && option);    
+      if (!data) {
+	    $this.data(oc, (data = new Application(this, options)));
+      }
+      if (typeof option == 'string') {
+        result = data[option].apply(data, args);
+      }            
+      if (typeof result != 'undefined') {
+        return false;
+      }
+	});
 
-        return result ? result : items;
-    };
+    return result ? result : items;
+  };
 
-    $.fn[appName].Constructor = Application;
+  $.fn[appName].Constructor = Application;
 
-    $.fn[appName].noConflict = function () {
-        $.fn[appName] = old;
-        return this;
-    };
+  $.fn[appName].noConflict = function () {
+    $.fn[appName] = old;
+    return this;
+  };
 
-    $(document).on('render', function (){
-        var $elems = $(appDataHandler);
-        $elems[appName]();
-    });
+  $(document).on('render', function (){
+    let $elems = $(appDataHandler);
+    $elems[appName]();
+  });
 }
 
 module.exports = Base;
